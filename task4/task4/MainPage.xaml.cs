c85b663ea210a8a112240f328e9e4e35edfea901
razanwael1122 <razanwael1122@gmail.com>
2023-06-20 11:15:10 +0300

    Add project files.

---
+++ task4/task4/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace task4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Button_Clicked(System.Object sender,System.EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions {
+            
+               Title="pick a photo"
+            
+            
+            });
+
+            var stream = await result.OpenReadAsync();
+            result_image.Source=ImageSource.FromStream(()=>stream);
+
+        }
+        async void Button1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync();
+            var stream = await result.OpenReadAsync();
+            result_image.Source = ImageSource.FromStream(() => stream);
+        }
+    }
+}
